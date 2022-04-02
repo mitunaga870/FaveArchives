1bf5a46431201e1c7707cf39ffadf3c0816482b7
mitunaga870 <af21022@shibaura-it.ac.jp>
2022-04-02 18:05:58 +0900

    youtube ifream api実装途中

--- generalfunc/mekeiframe.js
+++ generalfunc/mekeiframe.js
@@ -1,15 +1,7 @@
 module.exports = async (id,start=0) =>{
+    var player;
     function onYouTubeIframeAPIReady() {
-        console.log('a')
-        new YT.Player('video', {
-            videoId: id,
-            playerVars: {
-                autoplay:1,
-                enablejsapi:1,
-                modestbranding:1,
-                rel:0,
-                start:start
-            },
-        })
-    };
+
+    }
+
 }
